bf7c5d02420fc8a298a1515fcba313650859fa21
Aldren Terante <aldren.terante@salarium.com>
2016-09-19 15:05:04 +0800

    setup Behat with simple test cases

---
+++ api/features/bootstrap/FeatureContext.php
@@ -0,0 +1,36 @@
+<?php
+
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+#This will be needed if you require "behat/mink-selenium2-driver"
+#use Behat\Mink\Driver\Selenium2Driver;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    public function iShouldBeAbleToDoSomethingWithLaravel()
+    {
+        $environmentFileName = app()->environmentFile();
+        $environmentName = env('APP_ENV');
+        PHPUnit::assertEquals('.env.behat', $environmentFileName);
+        PHPUnit::assertEquals('acceptance', $environmentName);
+    }
+}
