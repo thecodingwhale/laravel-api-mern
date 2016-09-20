677d89353a20ebf5f10fd87d1a326ed325552fcd
Aldren Terante <aldren.terante@salarium.com>
2016-09-20 13:50:59 +0800

    add phpspec with sample test class

--- api/features/bootstrap/FeatureContext.php
+++ api/features/bootstrap/FeatureContext.php
@@ -9,7 +9,9 @@ use Behat\Gherkin\Node\TableNode;
 #This will be needed if you require "behat/mink-selenium2-driver"
 #use Behat\Mink\Driver\Selenium2Driver;
 use Behat\MinkExtension\Context\MinkContext;
+use PHPUnit_Framework_Assert as PHPUnit;
 
+use App\Calculator as Calculator;
 /**
  * Defines application features from the specific context.
  */
@@ -32,5 +34,22 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
         $environmentName = env('APP_ENV');
         PHPUnit::assertEquals('.env.behat', $environmentFileName);
         PHPUnit::assertEquals('acceptance', $environmentName);
+    }
+
+    /**
+     * @Given the method :arg1 receives the numbers :arg2 and :arg3
+     */
+    public function theMethodReceivesTheNumbersAnd($arg1, $arg2, $arg3)
+    {
+        $this->calculator = new Calculator();
+        $this->calculator->$arg1($arg2, $arg3);
+    }
+
+    /**
+     * @Then the calculated value should be :arg1
+     */
+    public function theCalculatedValueShouldBe($arg1)
+    {
+        PHPUnit::assertEquals($arg1, $this->calculator->result());
     }
 }
